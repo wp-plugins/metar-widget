343ff6fabce084d4d39fe4b8898e3a9da50e0872
mcantsin <mcantsin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-14 22:06:03 +0000

    Adding first version of metar-widget plugin
    git-svn-id: https://plugins.svn.wordpress.org/metar-widget/trunk@585548 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ MetarWidget.php
@@ -0,0 +1,49 @@
+<?php
+class MetarWidget extends WP_Widget
+{
+    public function __construct() {
+        parent::__construct("metar_widget", "METAR Widget",
+            array("description" => "Plugin to inlclude the latest METAR information from NOAA database for any Airport"));
+    }
+
+    public function form($instance) {
+        $icao = "";
+        // if instance is defined, populate the fields
+        if (!empty($instance)) {
+            $icao = $instance["icao"];
+        }
+
+        $tableId = $this->get_field_id("icao");
+        $tableName = $this->get_field_name("icao");
+        echo '<label for="' . $tableId . '">ICAO</label><br/>';
+        echo '<input id="' . $tableId . '" type="text" name="' .
+            $tableName . '" value="' . $icao . '"/><br/>';
+    }
+
+    public function update($newInstance, $oldInstance) {
+        $values = array();
+        $values["icao"] = htmlentities($newInstance["icao"]);
+        return $values;
+    }
+
+    public function widget($args, $instance) {
+        $icao = $instance["icao"];
+
+        $fileName = "http://weather.noaa.gov/pub/data/observations/metar/stations/$icao.TXT";
+        $metar = '';
+        $fileData = @file($fileName) or die('METAR not available');
+        if ($fileData != false) {
+        	list($i, $date) = each($fileData);
+        	$utc = strtotime(trim($date));
+        	$time = date("D, F jS Y g:i A",$utc);
+
+        	while (list($i, $line) = each($fileData)) {
+        		$metar .= ' ' . trim($line);
+            	}
+        	$metar = trim(str_replace('  ', ' ', $metar));
+        }
+
+	echo "<h4>Current METAR for $icao</h4>";
+        echo "<p id='metar'>$metar</p>";
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,43 @@
+=== metar-widget ===
+Contributors: mcantsin
+Author URI: http://lutherblissett.net
+Plugin URL: http://wordpress.org/extend/plugins/metar-widget/
+Tags: widget, metar, weather, noaa
+Requires at least: 3.4
+Tested up to: 3.4.1
+Stable tag: 0.1
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+A simple widget to display the current METAR code (Pilot weather code) for a chosen ICAO station.
+
+== Description ==
+
+METAR-widget lets you enter an ICAO station (airport) in the widget admin and will display the according METAR code as widget on your wordpress site.
+The METAR data used is provided by NOAA.
+
+== Installation ==
+
+1. Unzip metar-widget.zip
+2. Upload the `metar-widget` folder into the `/wp-content/plugins/` directory on the server
+3. Activate the plugin through the 'Plugins' menu in WordPress
+4. Place the metar-widget in the wordpress admin interface
+5. Enter an ICAO code for the respective airport (e.g. OMDB for Dubai, United Arab Emirates or VTSR for Siem Reap, Cambodia)
+
+== Frequently Asked Questions ==
+
+= What is METAR? =
+
+METAR is a format for reporting weather information. A METAR weather report is predominantly used by pilots in fulfillment of a part of a pre-flight weather briefing, and by meteorologists, who use aggregated METAR information to assist in weather forecasting.
+
+== Screenshots ==
+
+1. Screenshot of placed metar-widget on Cambodia vACC website
+
+== Changelog ==
+
+= 0.1 =
+initial version
+
+== Upgrade Notice ==
+
